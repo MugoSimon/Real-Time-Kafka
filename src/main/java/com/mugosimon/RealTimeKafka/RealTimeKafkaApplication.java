d8c523a54b8f8e65f411f993dbecab6dbb4a182a
Simon.Mugo <mugolastbon@gmail.com>
2024-02-21 14:36:32 +0300

    Initial commit

---
+++ src/main/java/com/mugosimon/RealTimeKafka/RealTimeKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.mugosimon.RealTimeKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugosimon/RealTimeKafka/RealTimeKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugosimon.RealTimeKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
