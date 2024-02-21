5fcfc8a04a54bdec57c6c6bd33b5d6fe5049f1bf
Simon.Mugo <mugolastbon@gmail.com>
2024-02-21 16:53:03 +0300

    inaboo

---
+++ kafka-producer-wikimedia/src/main/java/com/mugosimon/springboot/KafkaTopicConfig.java
@@ -0,0 +1,13 @@
+package com.mugosimon.springboot;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    public NewTopic topic(){
+        return TopicBuilder.name("wikimedia_recentChange")
+                .build();
+    }
+}
